1d6f1546e7db248f91f1a72c0a492500870a4e78
RoaaBaniOdeh <yosrmohyeebaniodeh@com>
2025-07-12 12:10:40 +0300

    yusr

---
+++ index.js
@@ -0,0 +1,11 @@
+//var h1Text= document .getElementById("heading")
+//h1text.innerText = "changed using JS just right now"
+
+function changeColor11(){
+    var colorText = document.getElementById("pcolor")
+     colorText.style.color ="red"
+}
+    
+
+
+
